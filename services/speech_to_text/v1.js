06cb46ba00bd95f9cca6968c2b80e65a0e7f3192
Chris Parsons <christopherkparsons@gmail.com>
2016-12-14 11:15:43 +0000

    removing ; bug

--- services/speech_to_text/v1.js
+++ services/speech_to_text/v1.js
@@ -149,7 +149,7 @@ module.exports = function (RED) {
               });
             });
           }
-          msg.serviceUTILS = serviceUtils.checkServiceBound('speech-to-text');;
+          msg.serviceUTILS = serviceUtils.checkServiceBound('speech-to-text');
           node.send(msg);
         }
       };
